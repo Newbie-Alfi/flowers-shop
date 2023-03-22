73813726f71f472a442229c53e504a0cccfb0b88
Newbie-Alfi <aleksandr20021008@gmail.com>
2023-03-22 20:56:20 +0300

    feat(client): add the map component

---
+++ client/src/shared/ui/Map/index.tsx
@@ -0,0 +1,11 @@
+export function Map() {
+  return (
+    <iframe
+      title="Карта"
+      src="https://yandex.ru/map-widget/v1/?um=constructor%3Ac704d651bd81d5d53a70376034570550e4311bfb8d75d3d9d42a3d78237f7893&amp;source=constructor"
+      width="100%"
+      height="400"
+      frameBorder={0}
+    />
+  );
+}
